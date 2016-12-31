d48c7951b1f37c04adae5ed6e101cc433384b1c6
B-Sarah <sboutahlil@gmail.com>
2016-12-31 12:27:14 +0100

    UI setup

--- LostHistory/war/app.js
+++ LostHistory/war/app.js
@@ -13,8 +13,13 @@ app.run(['GAuth', function(GAuth) {
 
 app.controller('MainController', ['$scope','GAuth', function($scope, GAuth) {
     $scope.user = null;
+    $scope.page = null;
+    $scope.user = null;
+    $scope.highscores = false;
+    $scope.loggedin = false;
     $scope.loggedin = false;
     $scope.textLogin = "Login";
+
     
     $scope.login = function() {
         GAuth.login().then(function(user) {
@@ -32,12 +37,89 @@ app.controller('MainController', ['$scope','GAuth', function($scope, GAuth) {
    
     
     $scope.logout = function() {
-        GAuth.signOut();
+        GAuth.logout();
         $scope.user = null;
         $scope.loggedin = false;
+        $scope.openPage('home');
         console.log("Bye .. ");
    
     };
+    
+    
+    
+    
+	$scope.answerClick = function(index) {
+		if ($scope.question.answered === false) {
+			$scope.question.answered = index;
+			$scope.questions.answered++;
+			if($scope.question.good_answer === $scope.question.answered) {
+				$scope.questions.well_answered++;
+			}
+		}
+	}
+
+	$scope.restart = function() {
+
+		$scope.questions = {
+			answered: 0,
+			well_answered: 0,
+		}
+
+		$scope.nextQuestion();
+	}
+
+	$scope.nextQuestion = function() {
+
+		if ($scope.questions.answered < 10) {
+
+			$scope.question = {
+				text: "Text of the question",
+				answers: [
+					"First answer",
+					"Second answer",
+					"Third answer",
+				],
+				good_answer: Math.floor(Math.random()*3),
+				answered: false
+			}
+
+		} else {
+			//$scope.insertScore({id: Math.floor(Math.random()*1000000)+1, score: $scope.questions.well_answered*10});
+			$scope.openPage('gameover');
+		}
+		
+	}
+
+	$scope.openPage = function(page) {
+		$scope.page = page;
+			if (page == 'play') {
+				if(!$scope.loggedin)
+					$scope.login();
+					$scope.restart();
+					
+				
+				
+			}
+			else if (page == 'highscores') {
+				$scope.highscores = null;
+				$scope.listScores(function(data) {
+				$scope.highscores = data;
+			});
+		}
+	   else if (page == 'home') {
+		}
+
+			
+	}		
+
+	$scope.listScores = function(callback) {
+		GApi.execute('scoreentityendpoint','listScoreEntity').then(function(resp) {
+			callback(resp.items);
+		}, function() {
+			console.log('Error!');
+		});
+	}
+	$scope.openPage('home');
     
 
      
